17a81c4eadff29f03d0342f81990f4ae6886ec03
drek226 <37354452+drek226@users.noreply.github.com>
2018-03-25 20:08:12 -0500

    p1/p4

--- src/P1_MysteryNums.java
+++ src/P1_MysteryNums.java
@@ -6,18 +6,30 @@
 
 /*
  1. Your output prediction:
-  
+  15,42
+  10,25
   
  */
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
+ // I was right but I just guessed "num1" and "num2" would print the answer of each sentence. 
  
  
  */
 
 public class P1_MysteryNums {
-
+	public static void main(String[] args) {
+        int x = 15;
+        sentence( x, 42 );
+        
+        int y = x - 5;
+        sentence( y, x + y);
+    }
+    
+    public static void sentence( int num1, int num2 ) {
+        System.out.println( num1 + " " + num2 );
+    }
 
 
 }
--- src/P4_MysteryTouch.java
+++ src/P4_MysteryTouch.java
@@ -5,7 +5,7 @@
 */
 
 /*
- 4. Your output prediction:
+ 4. Your output prediction: touch your head to your eye
   
   
  */
@@ -13,10 +13,30 @@
 /*
  6. Were you correct? Explain if you were not (what did you learn?)
  
+ Wow! I was way off on this one. Why are so many lines printed? I do not understand this assignment
+ 
  
  */
 
 public class P4_MysteryTouch {
+    public static void main(String[] args) {
+        String head = "shoulders";
+        String knees = "toes"; 
+        String elbow = "head";
+        String eye = "eyes and ears";
+        String ear = "eye"; 
+        
+        touch(ear, elbow);
+        touch(elbow, ear); 
+        touch(head, "elbow"); 
+        touch(eye, eye);
+        touch(knees, "Toes"); 
+        touch(head, "knees " + knees );
+    }
+    
+    public static void touch(String elbow, String ear) { 
+        System.out.println( "touch your " + elbow + " to your " + ear );
+    }
 
 
 
